f6c54fd9dd9c4b7caf2c5c83bdbcb137fc4af715
Matthew Chan <mchan393@uwo.ca>
2018-09-17 10:18:53 -0400

    did stuff

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <Calculator />
+    <p>Designed and Coded by <a class='link' href='https://www.freecodecamp.org/matthewhtc' target='_blank'>Matthew Chan</a>.</p>
   </div>
 </template>
 
@@ -16,12 +17,24 @@ export default {
 </script>
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Roboto', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+a.link, a.link:visited {
+  text-decoration: none; 
+}
+a.link:visited {
+  color: #2c3e50;
+}
+
+a.link:hover {
+  color: darken(#FF5722, 10%);
+  transition: .25s ease;
 }
 
 body, html {
--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -6,19 +6,19 @@
     <div @click='clear' class='btn'>C</div>
     <div @click='sign' class='btn'>+/-</div>
     <div @click='percent' class='btn'>%</div>
-    <div @click='divide' class='btn operator'>÷</div>
+    <div @click='operation("/")' class='btn operator'>÷</div>
     <div @click='append("7")' class='btn'>7</div>
     <div @click='append("8")' class='btn'>8</div>
     <div @click='append("9")' class='btn'>9</div>
-    <div @click='times' class='btn operator'>x</div>
+    <div @click='operation("×")' class='btn operator'>×</div>
     <div @click='append("4")' class='btn'>4</div>
     <div @click='append("5")' class='btn'>5</div>
     <div @click='append("6")' class='btn'>6</div>
-    <div @click='minus' class='btn operator'>-</div>
+    <div @click='operation("-")' class='btn operator'>-</div>
     <div @click='append("1")' class='btn'>1</div>
     <div @click='append("2")' class='btn'>2</div>
     <div @click='append("3")' class='btn'>3</div>
-    <div @click='add' class='btn operator'>+</div>
+    <div @click='operation("+")' class='btn operator'>+</div>
     <div @click='append("0")' class='zero btn' style='border-radius: 0 0 0 5px'>0</div>
     <div @click='dot' class='btn'>.</div>
     <div @click='equal' class='btn operator' style='border-radius: 0 0 5px 0'>=</div>
@@ -56,7 +56,7 @@ export default {
         this.operatorClicked = false;
       }
       this.previous = `${this.previous}${number}`
-      this.current = `${this.current}${number}` // more explicit that we are joining strings
+      this.current = `${this.current}${number}`
     },
     dot() {
       if (this.current.indexOf('.') === -1) {
@@ -64,41 +64,22 @@ export default {
       } 
     },
     setPrevious() {
-      this.previous = this.current;
       this.operatorClicked = true;
     },
-    divide() {
-      if (!this.operatorClicked) {
-        this.current = `${this.current}/`;
-        this.previous = `${this.previous}/`;
-        this.setPrevious();
+    operation(sign) {
+      if (this.previous.indexOf('=') !== -1) {
+        this.previous = this.current;
       }
-    },
-    times() {
       if (!this.operatorClicked) {
-        this.current = `${this.current}*`;
-        this.previous = `${this.previous}*`;
-        this.setPrevious();
-      }
-    },
-    minus() {
-      if (!this.operatorClicked) {
-        this.current = `${this.current}-`;
-        this.previous = `${this.previous}-`;
-        this.setPrevious();
-      }
-    },
-    add() {
-      if (!this.operatorClicked) {
-        this.current = `${this.current}+`;
-        this.previous = `${this.previous}+`;
+        this.current = `${this.current}${sign}`;
+        this.previous = `${this.previous}${sign}`;
         this.setPrevious();
       }
     },
     equal() {
-        this.current = `${eval(this.previous)}`;
-        this.previous = `${this.previous}=${eval(this.previous)}`;
-        this.operatorClicked = false;
+      this.current = `${eval(this.previous.replace('×', '*'))}`;
+      this.previous = `${this.previous}=${eval(this.previous.replace('×', '*'))}`;
+      this.operatorClicked = false;
     }
   }
 }
@@ -107,17 +88,18 @@ export default {
 <style scoped lang="scss">
 @import url('https://fonts.googleapis.com/css?family=Roboto');
 $orange: #FF5722;
+$blue-grey: #607D8B;
 
 .calculator {
   box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);
   width: 350px;
   margin: 0 auto;
   font-size: 25px;
-  font-family: 'Roboto', sans-serif;
   display: grid;
   grid-template-columns: repeat(4, 1fr); // 4 x infinity grid
   grid-auto-rows: minmax(65px, auto); // 50 px in height
   border-radius: 5px;
+  margin: 30px auto 30px;
 
   .display, .miniDisplay {
     grid-column: 1 / 5; // start at column 1, end after column 4
@@ -143,49 +125,16 @@ $orange: #FF5722;
   }
 
   .btn {
-      background-color: #607D8B;
+      background-color: $blue-grey;
       color: white;
       display: flex;
       justify-content: center;
       align-items: center;
-      overflow: hidden;
     }
 
-    @keyframes ripple {
-    0% {
-      transform: scale(0, 0);
-      opacity: 1;
-    }
-    20% {
-      transform: scale(25, 25);
-      opacity: 1;
-    }
-    100% {
-      opacity: 0;
-      transform: scale(40, 40);
-    }
-  }
-
-  .btn:after {
-    content: '';
-    top: 50%;
-    left: 50%;
-    width: 5px;
-    height: 5px;
-    background: rgba(96, 125, 139, 0.5);
-    opacity: 0;
-    border-radius: 100%;
-    transform: scale(1, 1) translate(-50%);
-    transform-origin: 50% 50%;
-  }
-
-  // .btn:not(:active)::after {
-  //   animation: ripple 1s ease-out;
-  // }
-  
   .btn:hover {
     cursor: pointer;
-    background-color: rgba(96, 125, 139, 0.8);
+    background-color: darken($blue-grey, 10%);
   }
 
   .operator {
